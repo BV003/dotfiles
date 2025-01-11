13d218d04a8102971af782eefed88b3223cf1975
BV003 <2022302111185@whu.edu.cn>
2025-01-11 17:34:28 +0800

    初步完成

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-# 通过符号链接安装所有配置文件
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-# 如果有其他配置文件，按同样的方法继续添加
-# ln -sf ~/dotfiles/.vimrc ~/.vimrc
+# 设置 dotfiles 的路径
+# DOTFILES_DIR=$(dirname "$(readlink -f "$0")")
 
-echo "配置文件安装完成！"
+# # 创建符号链接
+# ln -sf "$DOTFILES_DIR/bashrc" "$HOME/.bashrc"
+
+
+
+# 确保 shell 使用新的配置
+# source "$HOME/.bashrc"
+
+echo "配置文件已成功部署！"
+#!/bin/bash
+
+# 通过符号链接安装所有配置文件
+ln -sf ~/codehome/tmp/missing/5/dotfiles/.bashrc ~/.bashrc
+# # 如果有其他配置文件，按同样的方法继续添加
+
+# echo "配置文件安装完成！"
 
